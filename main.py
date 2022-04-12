05c15855417e3d0edacddb6831abca8dea0fc667
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-12 21:07:52 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,18 @@
+#OSA
+a = 2
+b = 0
+displej = 5
+for i in range(displej):
+    led.plot_brightness(a, b, 100)
+    b += 1
+
+#CTVERCE
+led.set_brightness(255)
+x = 0
+y = randint(0, 3)
+for i in range(2):
+    led.plot(x ,y)
+    led.plot(x+1 ,y)
+    led.plot(x ,y+1)
+    led.plot(x+1 ,y+1)
+    x += 3
---
+++ main.ts
@@ -0,0 +1,20 @@
+let i: number;
+// OSA
+let a = 2
+let b = 0
+let displej = 5
+for (i = 0; i < displej; i++) {
+    led.plotBrightness(a, b, 100)
+    b += 1
+}
+// CTVERCE
+led.setBrightness(255)
+let x = 0
+let y = randint(0, 3)
+for (i = 0; i < 2; i++) {
+    led.plot(x, y)
+    led.plot(x + 1, y)
+    led.plot(x, y + 1)
+    led.plot(x + 1, y + 1)
+    x += 3
+}
